4827b82f95b705895692744d166a5c82e7e7e144
samotazky <samotazky82@gmail.com>
2023-11-19 21:58:01 +0100

    one user

--- user.php
+++ user.php
@@ -1,3 +1,31 @@
+<?php
+
+    require "assets/db.php";
+
+    $connection = connectionDB();
+
+    if (isset($_GET["id"]) and is_numeric($_GET["id"])) {
+
+        $sql = "SELECT * 
+                FROM users
+                WHERE id = ?";
+        
+        $stmt = mysqli_prepare($connection, $sql);
+
+        if ($stmt === false) {
+            echo mysqli_error($connection);
+        } else {
+            mysqli_stmt_bind_param($stmt, "i", $_GET["id"]);
+
+            if (mysqli_stmt_execute($stmt)) {
+                $result = mysqli_stmt_get_result($stmt);
+                $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            }
+        }
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,5 +35,14 @@
 </head>
 <body>
     
+    <?php if ($user === null) : ?>
+        <p>User is not find</p>
+
+    <?php else : ?>
+        <h2><?= htmlspecialchars($user["first_name"]) ." " .htmlspecialchars($user["second_name"]) ?></h2>
+        <p><strong>Age:</strong> <?= htmlspecialchars($user["age"]) ?></p>
+        <p><strong>E-Mail:</strong> <?= htmlspecialchars($user["email"]) ?></p>
+    <?php endif ?>
+
 </body>
 </html>
